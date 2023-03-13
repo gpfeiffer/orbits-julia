6fa0a97f74b55cff0ef3c7b17ea0213d94739b03
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2023-03-13 08:11:45 +0000

    orbit, elements and orbit_with_words

--- julia/permutation.jl
+++ julia/permutation.jl
@@ -2,16 +2,20 @@
 ##
 module permutation
 
-import Base: length, inv, ==, <, *, ^
+import Base: length, inv, isless, one, ==, *, ^
 
 export Perm
-export degree, domain
+export degree, domain, transposition
 export p, q  # test data
 
 ##  Perm data type
 struct Perm
     list::Array{Integer}
 end
+
+## identity
+one(perm::Perm) = Perm(domain(perm))
+one(Perm, n::Integer) = Perm(1:n)
 
 ## degree
 degree(perm::Perm) = length(perm.list)
@@ -23,7 +27,7 @@ domain(perm::Perm) = 1:degree(perm)
 ==(perm::Perm, other::Perm) = perm.list == other.list
 
 ## comparison for size
-<(perm::Perm, other::Perm) = perm.list == other.list
+isless(perm::Perm, other::Perm) = perm.list < other.list
 
 ## point under perm
 ^(x::Integer, perm::Perm) = perm.list[x]
@@ -41,7 +45,7 @@ end
 ## power
 function ^(perm::Perm, n::Integer)
     n < 0 && return inv(perm)^(-n)
-    n == 0 && return Perm(domain(perm))
+    n == 0 && return one(perm)
     n == 1 && return perm
 
     q, r = divrem(n, 2)
@@ -53,6 +57,15 @@ end
 ## order??
 
 ## cycles??
+
+## transpositions
+function transposition(n::Integer, j::Integer, k::Integer)
+    list = collect(1:n)
+    list[[j, k]] = [k, j]
+    return Perm(list)
+end
+
+#transpositions(n::Integer) = [transposition(n, j-1, j) for j in 2:n]
 
 
 ## test data
